84c20e9392c81de1cd9e520cea3d3459b8813ce5
countervant <davidpeejay123@gmail.com>
2026-01-09 21:41:32 +0800

    dbconnection

--- Server/server.js
+++ Server/server.js
@@ -1,26 +1,16 @@
 import express from 'express';
-
-
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import dbConnect from './config/dbConnect.js';
 const app = express();
 dotenv.config();
 
 app.use(express.json());
 
-const mongoURI = process.env.MONGO_URI;
+dbConnect();
+
 const port = process.env.PORT
 
-mongoose.connect(mongoURI) .then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.error('Error connecting to MongoDB:', err);
-});
 
-async function adduser() {
-    
-}
 
 app.listen(5000, () => {
   console.log(`Server running on port 5000`);
