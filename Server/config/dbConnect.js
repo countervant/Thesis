3683114ea990c6608aea3bb6de478be2f9fc71f9
Peejay David <davidpeejay123@gmail.com>
2026-01-10 19:22:39 +0800

    database

--- Server/config/dbConnect.js
+++ Server/config/dbConnect.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
- try{
-     const connect = await mongoose.connect(process.env.MONGO_URI);
-     console.log(`MongoDB Connected: ${connect.connection.host}, ${connect.connection.name}`);
-} catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
- }
-};
 
-export default connectDB;
+export const dbConnect = async () => {
+    try {
+  const conn = await mongoose.connect(process.env.MONGODB_URI);
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    }catch (error) {
+        console.error("Database connection error:", error);
+        process.exit(1);
+    }
+}
--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import dbConnect from './config/dbConnect.js';
+import {dbConnect} from './config/dbConnect.js';
 const app = express();
 dotenv.config();
 
